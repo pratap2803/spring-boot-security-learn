46b7e31a2404d181da0b2ebeb141ff41def24c42
pratap2803 <saurabhsinghchauhan1507@gmail.com>
2024-01-11 17:25:55 +0530

    initial Commit

--- spring-boot-security-learn/src/main/java/com/learn/config/MySecurityConfig.java
+++ spring-boot-security-learn/src/main/java/com/learn/config/MySecurityConfig.java
@@ -3,28 +3,40 @@ package com.learn.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MySecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure (HttpSecurity http) throws Exception {
         http
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
+                .antMatchers("/signin").permitAll()
                 //.antMatchers("/home","/login", "/register").permitAll()
                 //.antMatchers("/public/**").permitAll()
                 .antMatchers("/public/**").hasRole("NORMAL")
                 .antMatchers("/users/**").hasRole("ADMIN")
+                /*
+                if we are enabling prePost annotation then we do not need
+                antMatchers()
+                 */
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+//                .httpBasic();
+                .formLogin()// for enabling form based login
+                .loginPage("/signin");
     }
 
     @Override
--- spring-boot-security-learn/src/main/java/com/learn/controllers/UserController.java
+++ spring-boot-security-learn/src/main/java/com/learn/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.learn.controllers;
 import com.learn.models.User;
 import com.learn.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,10 +20,13 @@ public class UserController {
     public List<User> getAllUsers(){
         return this.userService.getAllUsers();
     }
+
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{username}")
     public User getUserbyUsername(@PathVariable String username){
         return this.userService.getUser(username);
     }
+
     @PostMapping("/")
     public User addUser(@RequestBody User user){
         return this.userService.addUser(user);
